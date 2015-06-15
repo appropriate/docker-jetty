94d1d4bc7c494bf111b667fe2e740cef4ffe664e
Mike Dillon <mike@appropriate.io>
2015-06-15 14:06:00 -0700

    Update generate-stackbrew-library.sh to make 9.3 the default

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -3,19 +3,35 @@ set -ueo pipefail
 
 declare -A aliases
 aliases=(
-	[9.2-jre7]='latest jre7'
-	[9.2-jre8]='jre8'
+	[9.2-jre7]='jre7'
+	[9.3-jre8]='latest jre8'
 )
-defaultJava='7'
+defaultJava='8'
 defaultSuffix="jre${defaultJava}"
 
 cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
-paths=( */ )
-paths=( "${paths[@]%/}" )
+paths=( */Dockerfile )
+paths=( $( printf '%s\n' "${paths[@]%/Dockerfile}" | sort -Vr ) )
 url='git://github.com/appropriate/docker-jetty'
 
-echo '# maintainer: Mike Dillon <mike@embody.org> (@md5)'
+echo '# maintainer: Mike Dillon <mike@appropriate.io> (@md5)'
+
+declare -A tagsSeen
+tagsSeen=()
+outputTag() {
+	local tag="$1"
+	local url="$2"
+	local commit="$3"
+	local path="$4"
+
+	if [ ${#tagsSeen[$tag]} -gt 0 ]; then
+		return
+	fi
+
+	echo "$tag: ${url}@${commit} $path"
+	tagsSeen[$tag]=1
+}
 
 for path in "${paths[@]}"; do
 	echo
@@ -42,21 +58,21 @@ for path in "${paths[@]}"; do
 	# e.g. 9.2.10, 9.2, 9
 	if [ "$suffix" = "$defaultSuffix" ]; then
 		for va in "${versionAliases[@]}"; do
-			echo "$va: ${url}@${commit} $path"
+			outputTag "$va" "$url" "$commit" "$path"
 		done
 	fi
 
 	# Output ${versionAliases[@]} with suffixes
 	# e.g. 9.2.10-jre7, 9.2-jre7, 9-jre7
 	for va in "${versionAliases[@]}"; do
-		echo "$va-$suffix: ${url}@${commit} $path"
+		outputTag "$va-$suffix" "$url" "$commit" "$path"
 	done
 
 	# Output custom aliases
 	# e.g. latest, jre7, jre8
 	if [ ${#aliases[$path]} -gt 0 ]; then
 		for va in ${aliases[$path]}; do
-			echo "$va: ${url}@${commit} $path"
+			outputTag "$va" "$url" "$commit" "$path"
 		done
 	fi
 done
