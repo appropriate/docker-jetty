b2acbbd402e23c42329ada59c3a5d1d84188b98a
Greg Wilkins <gregw@webtide.com>
2017-08-16 16:21:30 +1000

    Issue #53 Bypass start.jar
    Rely on -e for non zero exit

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -81,13 +81,12 @@ if expr "$*" : 'java .*/start\.jar.*$' >/dev/null ; then
 	else
 		# Do a jetty dry run to set the final command
 		"$@" --dry-run > /jetty-start
-		EXIT=$?
 		if [ $(egrep -v '\\$' /jetty-start | wc -l ) -gt 1 ] ; then
 			# command was more than a dry-run
 			cat /jetty-start \
 			| awk '/\\$/ { printf "%s", substr($0, 1, length($0)-1); next } 1' \
 			| egrep -v '[^ ]*java .* org\.eclipse\.jetty\.xml\.XmlConfiguration '
-			exit $EXIT
+			exit
 		fi
 		set -- $(sed 's/\\$//' /jetty-start)
 	fi
